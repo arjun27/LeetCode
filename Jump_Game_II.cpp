4385384bfa768d1803f7df4b2384950eeba7bb41
Arjun Attam <arjunattam@gmail.com>
2013-09-28 19:45:01 +0530

    Jump Game II

--- Jump_Game_II.cpp
+++ Jump_Game_II.cpp
@@ -1,25 +1,26 @@
-// O(n^2) solution -> TLE for large inputs
+// tricky greedy solution!
 
 class Solution {
 public:
     int jump(int A[], int n) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-        vector <int> minJumps; minJumps.resize(n, -1);
-        minJumps[n-1] = 0;
-        int i = n-2;
-        int last = n-1;
-        while(i >= 0) {
-            int least = n+1;
-            for (int j = i+1; j <= i+A[i]; j++) {
-                if (minJumps[j] >= 0 && minJumps[j] < least) {
-                    least = minJumps[j];
+        if (n <= 1) return 0;
+        int i = 0, steps = 1;
+        while (i + A[i] < n-1) {
+            int j = i + A[i];
+            int max = j + A[j];
+            int rmax = j;
+            while (j > i) {
+                if (j + A[j] > max) {
+                    max = j + A[j];
+                    rmax = j;
                 }
+                j--;
             }
-            if (least < n+1) minJumps[i] = least+1;
-            else minJumps[i] = -1;
-            i--;
+            i = rmax;
+            steps++;
         }
-        return minJumps[0];
+        return steps;
     }
 };
--- README.md
+++ README.md
@@ -39,6 +39,7 @@ Solutions to LeetCode problems. I've tried to compile a list and classify proble
 * Reverse Linked List II `*`
 * Maximum Subarray
 * Jump Game
+* Jump Game `*`
 
 ### Dynamic Programming
 * Unique Paths
